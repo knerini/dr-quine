c9a3831b60c29d196ec1133a99b620d84bac2cad
knerini <nerini.krystel@outlook.com>
2025-01-12 22:37:44 +0100

    Update : Grace.cpp add the newline at the end of the file for more clarity at the output

--- CPP/Grace.cpp
+++ CPP/Grace.cpp
@@ -12,6 +12,7 @@ We're not in Blade Runner
 #define F_NAME "Grace_kid.cpp"
 #define SRC R"{}"
 #define FT() int main(){std::string s = SRC; s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')'); std::ofstream file; file.open(F_NAME); file << s; file.close(); return 0;}
-FT())"
+FT()
+)"
 #define FT() int main(){std::string s = SRC; s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')'); std::ofstream file; file.open(F_NAME); file << s; file.close(); return 0;}
 FT()
