c461288bf575ea047126626eb82405f7d2265ca5
knerini <nerini.krystel@outlook.com>
2025-01-12 16:29:53 +0100

    New feature : Sully quine in C and ASM

---
+++ ASM/Sully.s
@@ -0,0 +1,122 @@
+section .note.GNU-stack
+%macro FIND_REPLACE_X 1
+lea rdi,[rel %1]
+xor r8,r8
+%%find_X:
+cmp [rdi+r8],byte 88
+je %%replace
+inc r8
+jmp %%find_X
+%%replace:
+mov byte [rdi+r8], r15b
+%endmacro
+%macro WRITE_FD 2
+mov rax,1
+mov rdi,%1
+mov rdx,%2
+syscall
+%endmacro
+%macro LOAD_CHAR 1
+mov [rel tmp_c],byte %1
+lea rsi, [rel tmp_c]
+jmp .print
+%endmacro
+%macro EXEC 2
+xor rax,rax
+mov rax,57
+syscall
+test rax,rax
+jz %%child
+mov rdi,1
+mov rsi,rax
+xor rdx,rdx
+mov r10,4
+xor rax,rax
+mov rax,247
+syscall
+jmp %%next_process
+%%child:
+lea rdi,[rel %1]
+lea rsi,[rel %2]
+xor rdx,rdx
+mov rax,59
+syscall
+%%next_process:
+%endmacro
+section .data
+num dw 5
+exec db "Sully_X.s",0
+prog db "Sully_X",0
+obj db "Sully_X.o",0
+ex_prog db "./Sully_X",0
+nasm db "/usr/bin/nasm",0
+nasm_opt db "-f",0
+nasm_fmt db "elf64",0
+gcc db "/usr/bin/gcc",0
+ld db "-B/usr/bin",0
+gcc_opt db "-o",0
+cmd1 dq nasm,nasm_opt,nasm_fmt,exec,0
+cmd2 dq gcc,ld,gcc_opt,prog,obj,0
+cmd3 dq ex_prog,0
+s db "section .note.GNU-stack*%macro FIND_REPLACE_X 1*lea rdi,[rel %1]*xor r8,r8*%%find_X:*cmp [rdi+r8],byte 88*je %%replace*inc r8*jmp %%find_X*%%replace:*mov byte [rdi+r8], r15b*%endmacro*%macro WRITE_FD 2*mov rax,1*mov rdi,%1*mov rdx,%2*syscall*%endmacro*%macro LOAD_CHAR 1*mov [rel tmp_c],byte %1*lea rsi, [rel tmp_c]*jmp .print*%endmacro*%macro EXEC 2*xor rax,rax*mov rax,57*syscall*test rax,rax*jz %%child*mov rdi,1*mov rsi,rax*xor rdx,rdx*mov r10,4*xor rax,rax*mov rax,247*syscall*jmp %%next_process*%%child:*lea rdi,[rel %1]*lea rsi,[rel %2]*xor rdx,rdx*mov rax,59*syscall*%%next_process:*%endmacro*section .data*num dw @*exec db #Sully_X.s#,0*prog db #Sully_X#,0*obj db #Sully_X.o#,0*ex_prog db #./Sully_X#,0*nasm db #/usr/bin/nasm#,0*nasm_opt db #-f#,0*nasm_fmt db #elf64#,0*gcc db #/usr/bin/gcc#,0*ld db #-B/usr/bin#,0*gcc_opt db #-o#,0*cmd1 dq nasm,nasm_opt,nasm_fmt,exec,0*cmd2 dq gcc,ld,gcc_opt,prog,obj,0*cmd3 dq ex_prog,0*s db #^#,0*tmp_c db 0*section .text*global main*main:*cmp word [rel num],0*jle .end*mov r15w,word [rel num]*dec r15w*add r15b,&0&*FIND_REPLACE_X exec*mov rax,85*mov rsi,00400*syscall*mov r14,rax*FIND_REPLACE_X prog*FIND_REPLACE_X obj*FIND_REPLACE_X ex_prog*lea r12,[rel s]*.loop:*mov al,byte [r12]*cmp al,0*je .exec_step*cmp al,byte 42*je .newline*cmp al,byte 35*je .dquote*cmp al,byte 38*je .squote*cmp al,byte 64*je .nbr*cmp al,byte 94*je .string*lea rsi,[r12]*jmp .print*.newline:*LOAD_CHAR 10*.dquote:*LOAD_CHAR 34*.squote:*LOAD_CHAR 39*.nbr:*mov [rel tmp_c],r15b*lea rsi,[rel tmp_c]*jmp .print*.string:*lea rsi,[rel s]*WRITE_FD r14,1738*jmp .increment*.print:*WRITE_FD r14,1*.increment:*add r12,1*jmp .loop*.exec_step:*EXEC nasm,cmd1*EXEC gcc,cmd2*EXEC ex_prog,cmd3*.end:*mov rax,60*mov rdi,0*syscall*",0
+tmp_c db 0
+section .text
+global main
+main:
+cmp word [rel num],0
+jle .end
+mov r15w,word [rel num]
+dec r15w
+add r15b,'0'
+FIND_REPLACE_X exec
+mov rax,85
+mov rsi,00400
+syscall
+mov r14,rax
+FIND_REPLACE_X prog
+FIND_REPLACE_X obj
+FIND_REPLACE_X ex_prog
+lea r12,[rel s]
+.loop:
+mov al,byte [r12]
+cmp al,0
+je .exec_step
+cmp al,byte 42
+je .newline
+cmp al,byte 35
+je .dquote
+cmp al,byte 38
+je .squote
+cmp al,byte 64
+je .nbr
+cmp al,byte 94
+je .string
+lea rsi,[r12]
+jmp .print
+.newline:
+LOAD_CHAR 10
+.dquote:
+LOAD_CHAR 34
+.squote:
+LOAD_CHAR 39
+.nbr:
+mov [rel tmp_c],r15b
+lea rsi,[rel tmp_c]
+jmp .print
+.string:
+lea rsi,[rel s]
+WRITE_FD r14,1738
+jmp .increment
+.print:
+WRITE_FD r14,1
+.increment:
+add r12,1
+jmp .loop
+.exec_step:
+EXEC nasm,cmd1
+EXEC gcc,cmd2
+EXEC ex_prog,cmd3
+.end:
+mov rax,60
+mov rdi,0
+syscall
---
+++ C/Sully.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+int main(){
+int i=5;
+if (i <= 0){return 0;}
+char filename[11];
+char cmd[40];
+const char s[] = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <stdlib.h>%1$cint main(){%1$cint i=%2$d;%1$cif (i <= 0){return 0;}%1$cchar filename[11];%1$cchar cmd[40];%1$cconst char s[] = %3$c%4$s%3$c;%1$csprintf(filename, %3$cSully_%5$cd.c%3$c, i-1);%1$csprintf(cmd, %3$cgcc -o Sully_%5$c1$d Sully_%5$c1$d.c ; ./Sully_%5$c1$d%3$c, i-1);%1$cint fd = creat(filename, S_IRUSR);%1$cif (fd){dprintf(fd, s, 10, i-1, 34, s, 37);}%1$csystem(cmd);%1$creturn 0;}%1$c";
+sprintf(filename, "Sully_%d.c", i-1);
+sprintf(cmd, "gcc -o Sully_%1$d Sully_%1$d.c ; ./Sully_%1$d", i-1);
+int fd = creat(filename, S_IRUSR);
+if (fd){dprintf(fd, s, 10, i-1, 34, s, 37);}
+system(cmd);
+return 0;}
