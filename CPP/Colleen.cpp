95d37c190a5c23ad74a58ac847ea65423edabe82
knerini <nerini.krystel@outlook.com>
2025-01-12 20:31:25 +0100

    new feature : Colleen quine in CPP OK

---
+++ CPP/Colleen.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <format>
+/*
+Is Rick Deckard a replicant ?
+*/
+void printer(const std::string& s){std::cout << s;}
+int main(){
+/*
+Rachael is a Nexus-6 replicant.
+*/
+std::string s = R"(#include <iostream>
+#include <format>
+/*
+Is Rick Deckard a replicant ?
+*/
+void printer(const std::string& s){std::cout << s;}
+int main(){
+/*
+Rachael is a Nexus-6 replicant.
+*/
+std::string s = R"{}";
+s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')');
+printer(s);
+return 0;}
+)";
+s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')');
+printer(s);
+return 0;}
