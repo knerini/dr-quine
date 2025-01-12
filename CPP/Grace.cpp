05461c00e8323c42127402fe88745f50e07d3a9d
knerini <nerini.krystel@outlook.com>
2025-01-12 22:34:07 +0100

    New feature : Grace quine in CPP OK

---
+++ CPP/Grace.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+/*
+We're not in Blade Runner
+*/
+#define F_NAME "Grace_kid.cpp"
+#define SRC R"(#include <fstream>
+#include <iostream>
+/*
+We're not in Blade Runner
+*/
+#define F_NAME "Grace_kid.cpp"
+#define SRC R"{}"
+#define FT() int main(){std::string s = SRC; s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')'); std::ofstream file; file.open(F_NAME); file << s; file.close(); return 0;}
+FT())"
+#define FT() int main(){std::string s = SRC; s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')'); std::ofstream file; file.open(F_NAME); file << s; file.close(); return 0;}
+FT()
