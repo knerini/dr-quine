55e8d4c5b4bebbcde89cd69c92159837edb5216d
knerini <nerini.krystel@outlook.com>
2025-03-30 14:57:07 +0200

    Fix: creation of Sully_5 file like asked (not clearly) by the subject

--- CPP/Sully.cpp
+++ CPP/Sully.cpp
@@ -1,38 +1,42 @@
 #include <fstream>
 #include <iostream>
-#include <format>
 #include <cstdlib>
 int main(){
 int i = 5;
-if (i <= 0){return 0;}
+if (i < 0){return 0;}
 std::string s = R"(#include <fstream>
 #include <iostream>
-#include <format>
 #include <cstdlib>
 int main(){
 int i = ?;
-if (i <= 0){return 0;}
+if (i < 0){return 0;}
 std::string s = R"{}";
 s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')');
 s.replace(s.find("?"), std::string("?").length(), std::to_string(i-1));
-std::string f_name = "Sully_" + std::to_string(i-1) + ".cpp";
+std::string f_name = "Sully_" + std::to_string(i) + ".cpp";
 std::ofstream file;
 file.open(f_name);
 file << s;
 file.close();
-std::string cmd = std::format("clang++ -std=c++23 -o Sully_{0} Sully_{0}.cpp ; ./Sully_{0}", std::to_string(i-1));
+std::string cmd = "clang++ -std=c++23 -o Sully_{0} Sully_{0}.cpp ; ./Sully_{0}";
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
 std::system(cmd.c_str());
 return 0;
 }
 )";
 s.replace(s.find("{}"), std::string("{}").length(), '(' + s + ')');
 s.replace(s.find("?"), std::string("?").length(), std::to_string(i-1));
-std::string f_name = "Sully_" + std::to_string(i-1) + ".cpp";
+std::string f_name = "Sully_" + std::to_string(i) + ".cpp";
 std::ofstream file;
 file.open(f_name);
 file << s;
 file.close();
-std::string cmd = std::format("clang++ -std=c++23 -o Sully_{0} Sully_{0}.cpp ; ./Sully_{0}", std::to_string(i-1));
+std::string cmd = "clang++ -std=c++23 -o Sully_{0} Sully_{0}.cpp ; ./Sully_{0}";
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
+cmd.replace(cmd.find("{0}"), std::string("{0}").length(), std::to_string(i));
 std::system(cmd.c_str());
 return 0;
 }
